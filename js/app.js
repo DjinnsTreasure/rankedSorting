e7ba43590ad60a5ff4fae56b6e19997c59f7382e
Teagan Clayton <clay0063@algonquinlive.com>
2023-03-08 16:48:34 -0500

    Added splat zones

--- js/app.js
+++ js/app.js
@@ -8,8 +8,12 @@ APP = {
     addEventListener: () => {
         let btnSeries = document.getElementById('fetchSeries');
         let btnOpen = document.getElementById('fetchOpen');
+        let btnZonesSeries = document.getElementById('fetchZonesSeries');
+        let btnZonesOpen = document.getElementById('fetchZonesOpen');
         btnSeries.addEventListener('click', APP.displaySeries);
         btnOpen.addEventListener('click', APP.displayOpen);
+        btnZonesSeries.addEventListener('click', APP.displayZonesSeries);
+        btnZonesOpen.addEventListener('click', APP.displayZonesOpen);
     },
     fetchData: () => {
         let url = 'https://splatoon3.ink/data/schedules.json'
@@ -34,14 +38,67 @@ APP = {
             settings: schedule.bankaraMatchSettings.find(setting => setting.mode === 'CHALLENGE') || null
         }))
         .filter(schedule => schedule.settings !== null);
-        console.log(challengeBattles);
-        let ul = document.getElementById('displaySeries');
+        const ul = document.getElementById('displaySeries');
+        const fetchAll = true;
 
-        challengeBattles.forEach(el => {
+        APP.displayContent(challengeBattles, ul, fetchAll);
+    },
+    displayOpen: () => {
+        const openBattles = APP.matchList
+        .map(schedule => ({
+            startTime: schedule.startTime,
+            endTime: schedule.endTime,
+            settings: schedule.bankaraMatchSettings.find(setting => setting.mode === 'OPEN') || null
+        }))
+        .filter(schedule => schedule.settings !== null);
+        const ul = document.getElementById('displayOpen');
+        const fetchAll = true;
+
+        APP.displayContent(openBattles, ul, fetchAll);
+    },
+
+    displayZonesSeries: () => {
+        const challengeBattles = APP.matchList
+        .map(schedule => ({
+            startTime: schedule.startTime,
+            endTime: schedule.endTime,
+            settings: schedule.bankaraMatchSettings.find(setting => setting.mode === 'CHALLENGE') || null
+        }))
+        .filter(schedule => schedule.settings !== null)
+        .filter(schedule => schedule.settings.vsRule.rule === 'AREA');
+        
+        const ul = document.getElementById('displayZonesSeries');
+        const fetchAll = false;
+
+        APP.displayContent(challengeBattles, ul, fetchAll);
+    },
+    displayZonesOpen: () => {
+        const openBattles = APP.matchList
+        .map(schedule => ({
+            startTime: schedule.startTime,
+            endTime: schedule.endTime,
+            settings: schedule.bankaraMatchSettings.find(setting => setting.mode === 'OPEN') || null
+        }))
+        .filter(schedule => schedule.settings !== null)
+        .filter(schedule => schedule.settings.vsRule.rule === 'AREA');
+        
+        const ul = document.getElementById('displayZonesOpen');
+        const fetchAll = false;
+
+        APP.displayContent(openBattles, ul, fetchAll);
+    },
+
+    displayContent: (data, ul, fetchAll) => {
+        data.forEach(el => {
             let startDate = APP.convertDate(el.startTime);
             let endDate = APP.convertDate(el.endTime);
             const li = document.createElement('li');
-            li.textContent = `MODE: ${el.settings.vsRule.name} - STAGES: ${el.settings.vsStages[0].name} & ${el.settings.vsStages[1].name} - FROM: ${startDate} to ${endDate}`;
+            if (fetchAll === true)
+            {
+                li.textContent = `MODE: ${el.settings.vsRule.name} - STAGES: ${el.settings.vsStages[0].name} & ${el.settings.vsStages[1].name} - FROM: ${startDate} to ${endDate}`;
+            } else {
+                li.textContent = `STAGES: ${el.settings.vsStages[0].name} & ${el.settings.vsStages[1].name} - FROM: ${startDate} to ${endDate}`;
+            }
             ul.appendChild(li);
             console.log(el);
         });
@@ -49,20 +106,14 @@ APP = {
 
     convertDate: (importedDate) => {
         const dateTimeString = importedDate;
-
-        // Create a new Date object from the string
         const dateTime = new Date(dateTimeString);
-
-        // Get the month, day, and hour components from the Date object
+        
         const month = dateTime.toLocaleString('en-US', { month: 'long' });
         const day = dateTime.getDate();
         let hour = dateTime.getHours();
         const amOrPm = hour >= 12 ? 'pm' : 'am';
-
-        // Convert the hour component to 12-hour format
+        
         hour = hour % 12 || 12;
-
-        // Construct the formatted string
         const formattedString = `${month} ${day}, ${hour}${amOrPm}`;
         return formattedString;
     }
@@ -71,10 +122,9 @@ APP = {
 
 document.addEventListener('DOMContentLoaded', ()=>{
    APP.init();
-
+})
 
 //Tower control = LOFT
 //Rainmaker = GOAL
 //Clam blitz = CLAM
 //Splat zones = AREA
-})
